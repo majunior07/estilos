bbc89121271c63e6f7f4e7ff197d52dfb90f0f2a
Mauricio Augusto Junior | Hospital Unimed Poços de Caldas <email@email.com>
2023-03-15 18:55:05 -0300

    Terceiro commit 15/03/23

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import './App.css';
+//import './App.css';
+import { Container, Head, Titulo } from './styles';
 
 function App() {
   return (
-    <div className="container">
-      <header>
-        <a>Projeto Styled</a>
-      </header>     
-    </div>
+    <Container>
+        <Head>
+            <Titulo>Projeto Styled</Titulo>
+        </Head>   
+
+    </Container>
   );
 }
 
 export default App;
+
+
+
+
+
+/*
+  <div className="container">
+    <header className="header">
+      <a className="titulo">Projeto Styled</a>
+    </header>     
+    <h1>Bem vindo ao sistema!</h1>
+  </div>
+ */
 
 
 
---
+++ src/styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Container = styled.div` 
+display: flex;
+flex-direction: column;
+width: 100%;
+height: 500px;
+`;
+
+
+export const Head = styled.header`
+  width: 100%;
+  height: 70px;
+  background-color: brown;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+`;
+
+export const Titulo = styled.a`
+font-size: 25px;
+color: #00FF00;
+`;
